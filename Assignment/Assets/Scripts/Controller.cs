8a3cbb27ac01ec1f8398866187c71e4a6fa5f154
C17384436 <48553721+C17384436@users.noreply.github.com>
2020-12-31 12:48:34 +0000

    Final Submission

---
+++ Assignment/Assets/Scripts/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //Setting variables with speeds for the three types of character movement
+
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+
+    //Setting variables for mouse control and look rate speed
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
+
+    //Setting variables for rotation speed
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //defining the input of the mouse look controlls
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        //setting the distance of x and y for the mouse distance
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        //clamping the mouse
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        //Assigning the Roll axis in order to control character rotation
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        //rotating the character
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        //Assigning the Vertical axis in order to control character's forward and backwards movement
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+
+        //Assigning the Horizontal axis in order to control character going left and right
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+
+        //Assigning the Hover axis in order to control character going up and down
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+
+       
+    }
+}
---
+++ Assignment/Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    //Variable to establish height and detail of the plain
+    int heightScale = 5;
+    float detailScale = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When the script is called, it creates a mesh and uses the PerlinNoise feature in order to transform the
+        //plain from a flat surface to a randomly generated bumpy plain in order to make it look similar to a mountain
+        //pass
+
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for(int v = 0; v < vertices.Length; v++)
+        {
+            vertices[v].y = Mathf.PerlinNoise((vertices[v].x + this.transform.position.x) / detailScale, 
+                                              (vertices[v].z + this.transform.position.z) / detailScale) * heightScale;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
